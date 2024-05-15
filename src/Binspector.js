2c574664c27ecd74516e991b33be926506049dec
이재학 <jaehak.lee@seculetter.com>
2024-05-15 20:28:07 +0900

    trivial commit

---
+++ src/Binspector.js
@@ -0,0 +1,13 @@
+import './Binspector.css';
+
+function Binspector() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        MAIN
+      </header>
+    </div>
+  );
+}
+
+export default Binspector;
